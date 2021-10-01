fed83935eb9cc4284deca6579db2d3b1bad47dbc
Camilo_Ts <camil.tapias@gmail.com>
2021-10-01 13:34:51 -0300

    Se ha separado el header de la pagina principal para formar un nuevo componente llamado header-principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PrincipalComponent } from './pages/home/principal.component';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { HeaderPrincipalComponent } from './components/headerPrincipal/header-principal.component';
 
 @NgModule({
-  declarations: [AppComponent, PrincipalComponent, TabsComponent],
+  declarations: [AppComponent, PrincipalComponent, TabsComponent, HeaderPrincipalComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ src/app/components/headerPrincipal/header-principal.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'header-principal',
+    templateUrl:'./header-principal.component.html',
+    styleUrls:['./header-principal.component.css']
+})
+
+export class HeaderPrincipalComponent {
+    constructor() {
+        
+    }
+}

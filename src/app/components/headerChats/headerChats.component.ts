4d74832e64820f780c545342f6419f99061000e9
Camilo_Ts <camil.tapias@gmail.com>
2021-12-04 16:23:44 -0300

    Detalles

--- src/app/components/headerChats/headerChats.component.ts
+++ src/app/components/headerChats/headerChats.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector:'headerChats',
@@ -7,7 +8,16 @@ import {Component} from '@angular/core';
 })
 
 export class HeaderChatsComponent {
-    constructor() {
+
+    public ruta:string = "proyectoMenu/"
+    constructor(private nav:Router) {
         
+    }
+    ngOnInit(){
+        this.ruta = this.ruta + window.localStorage.getItem("idProyecto")
+    }
+
+    public irA(){
+        this.nav.navigate(["proyectoMenu/"+window.localStorage.getItem("idProyecto")])
     }
 }
